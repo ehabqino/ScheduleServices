caaed8e70e3c8c92d94f0d6a5e03d125298e5891
ehab qino <ehabqino@gmail.com>
2021-08-17 09:19:41 +0300

    commit "user screen part4"

--- src/js/viewModels/users.js
+++ src/js/viewModels/users.js
@@ -32,6 +32,8 @@ define([
       new ArrayDataProvider(self.usersArray, { idAttribute: "user_name" })
     );
 
+    self.messagesDataprovider = ko.observableArray([]);
+
     self.userName = ko.observable();
     self.userPassword = ko.observable();
     self.userDisplayName = ko.observable();
@@ -47,13 +49,10 @@ define([
         }
       });
     }; //end refreshAllData
-    //==========================================================
+    //==================================================================================
     self.refreshAllData();
 
-    //==========================================================
-
-    self.save = (event) => {};
-    //====================================================================================
+    //===================================================================================
     self.cancel = (event) => {
       document.getElementById("modalDialog1").close();
     }; //end cancel
@@ -63,12 +62,87 @@ define([
       self.userPassword("");
       self.userDisplayName("");
       self.userType("USER");
-      self.userActive("");
+      //self.userActive("");
       document.getElementById("modalDialog1").open();
     }; //end open
     //====================================================================================
 
-    //##########################################################
+    self.save = (event) => {
+      document.getElementById("modalDialog1").close();
+      if (
+        self.userName() == "" ||
+        self.userPassword() == "" ||
+        this.userDisplayName() == ""
+      ) {
+        self.messagesDataprovider.push({
+          severity: "error",
+          summary: "Error",
+          detail: "Error Add New User",
+          //autoTimeout: 2000,
+          autoTimeout: UTIL.message_timeout,
+        });
+      } //end if
+      else {
+        UsersModel.addUser(
+          self.userName(),
+          self.userPassword(),
+          self.userDisplayName(),
+          self.userType(),
+          self.userActive(),
+          (success, response) => {
+            // alert(success,response);
+            // console.log(response);
+
+            if (success) {
+              self.messagesDataprovider.push({
+                severity: "confirmation",
+                summary: "New User",
+                detail: "New User Added Successfuly",
+                //autoTimeout: 2000,
+                autoTimeout: UTIL.message_timeout
+
+              });
+              self.refreshAllData();
+            } //end if
+            //self.refreshAllData();
+          }
+        );
+      } //end else
+    }; //end save;
+    //====================================================================================
+
+    self.openUpdate = (event) => {
+      document.getElementById("updateUserDialogId").open();
+    }
+    //====================================================================================
+    self.delete_User = (event) => {
+
+      document.getElementById("deleteUserDialogId").open();
+
+    }
+    //===================================================================================
+
+    self.cancelDelete = (event) => {
+      document.getElementById("deleteUserDialogId").close();
+    }//end cancelDelete
+    //====================================================================================
+
+    self.okDelete = (event) => {
+      //console.log(self.serviceid());
+      document.getElementById("deleteUserDialogId").close();
+    }//end okDelete
+    //=====================================================================================
+    self.saveUpdate = (event) => {
+      //console.log(self.serviceid());
+      document.getElementById("updateUserDialogId").close();
+    }//end saveUpdate
+    //====================================================================================
+    self.cancelUpdate = (event) => {
+      document.getElementById("updateUserDialogId").close();
+    }//end cancelUpdate
+    //====================================================================================
+
+    //###################################################################################
   } //end UsersViewModel
 
   return UsersViewModel;
